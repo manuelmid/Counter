7ac424938eb4848e6195cc0698b8323f112d241b
Manuel Duran <manuelmid99@gmail.com>
2022-10-30 15:05:39 +0100

    Input controller added, button styles added

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 var dateString = new Date().toLocaleDateString();
-var input = document.getElementById("inp");
-var boton = document.getElementById("boton");
+var input = document.querySelector(".input-12");//Añadir aqui el input
+var botones = document.querySelectorAll("button");
 
 
 var listener = function(e) {
     var currentValue = localStorage.getItem(dateString);
-
-    if (currentValue) {
-        var currentValueParsed = parseInt(currentValue);
-        var inputNm = parseFloat(input.value);
-        currentValueParsed += inputNm;
-        localStorage.setItem(dateString, currentValueParsed);
-    } else {
-        localStorage.setItem(dateString, 1);
+    var inputNm = parseFloat(input.value);
+    
+        if(inputNm){
+            if (currentValue) {
+                var currentValueParsed = parseInt(currentValue);
+                currentValueParsed += inputNm;
+                localStorage.setItem(dateString, currentValueParsed);
+            }
+            else {
+                localStorage.setItem(dateString, inputNm);
+            }
     }
 }
 
-//document.addEventListener("click", listener);
+for( i = 0 ; i < botones.length ; i++ ){ botones[i].style.width = "100px"; botones[i].style.height = "100px"}
 
-boton.addEventListener("click", listener)
+document.addEventListener("keypress", function(e){
+
+    if(e.key === "Enter"){
+        listener();
+    }
+
+})
+
